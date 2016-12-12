25f6caa541ff7589f3c8d4c5ee88b1b70b8882d9
evenorog <evenorog@gmail.com>
2016-12-12 12:11:37 +0100

    Add documentation and fix bug in redo method

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
 //! A Undo/Redo library.
 //!
+//! It uses the [Command Pattern](https://en.wikipedia.org/wiki/Command_pattern) where the user
+//! implements the `UndoCmd` trait for each command and then the commands can be used with the
+//! `UndoStack`.
+//!
+//! The `UndoStack` has two different states, the clean state and the dirty state. The `UndoStack`
+//! is in a clean state when there are no more commands that can be redone, otherwise it's in a dirty
+//! state.
+//!
 //! # Example
 //! ```
 //! use std::rc::Rc;
@@ -37,7 +45,7 @@
 //!
 //! assert_eq!(vec.borrow().len(), 7);
 //!
-//! undo_stack.undo();
+//! undo_stack.undo(); // on_dirty is going to be called here.
 //! undo_stack.undo();
 //! undo_stack.undo();
 //!
@@ -59,14 +67,17 @@ pub trait UndoCmd {
 /// set methods that should be called for either state change. This is useful for example if
 /// you want to automatically enable or disable undo or redo buttons based on there are any
 /// more actions that can be undone or redone.
-pub struct UndoStack<T: UndoCmd> {
+///
+/// Note: An empty `UndoStack` is clean, so the first push will not trigger the clean method.
+pub struct UndoStack<'a, T: UndoCmd> {
     stack: Vec<T>,
     len: usize,
-    on_clean: Option<Box<FnMut()>>,
-    on_dirty: Option<Box<FnMut()>>,
+    on_clean: Option<Box<FnMut() + 'a>>,
+    on_dirty: Option<Box<FnMut() + 'a>>,
 }
 
-impl<T: UndoCmd> UndoStack<T> {
+impl<'a, T: UndoCmd> UndoStack<'a, T> {
+    /// Creates a new `UndoStack`.
     pub fn new() -> Self {
         UndoStack {
             stack: Vec::new(),
@@ -76,6 +87,7 @@ impl<T: UndoCmd> UndoStack<T> {
         }
     }
 
+    /// Creates a new `UndoStack` with the specified capacity.
     pub fn with_capacity(capacity: usize) -> Self {
         UndoStack {
             stack: Vec::with_capacity(capacity),
@@ -85,47 +97,58 @@ impl<T: UndoCmd> UndoStack<T> {
         }
     }
 
-    pub fn on_clean<F: FnMut() + 'static>(mut self, f: F) -> Self {
+    /// Sets what should happen if the state changes from dirty to clean.
+    /// By default the `UndoStack` does nothing when the state changes.
+    /// Consumes the `UndoStack` so this method should be called when creating the `UndoStack`.
+    pub fn on_clean<F: FnMut() + 'a>(mut self, f: F) -> Self {
         self.on_clean = Some(Box::new(f));
         self
     }
 
-    pub fn on_dirty<F: FnMut() + 'static>(mut self, f: F) -> Self {
+    /// Sets what should happen if the state changes from clean to dirty.
+    /// By default the `UndoStack` does nothing when the state changes.
+    /// Consumes the `UndoStack` so this method should be called when creating the `UndoStack`.
+    pub fn on_dirty<F: FnMut() + 'a>(mut self, f: F) -> Self {
         self.on_dirty = Some(Box::new(f));
         self
     }
 
+    /// Returns true if the state of `UndoStack` is clean.
     pub fn is_clean(&self) -> bool {
         self.len == self.stack.len()
     }
 
+    /// Returns true if the state of `UndoStack` is dirty.
     pub fn is_dirty(&self) -> bool {
         !self.is_clean()
     }
 
+    /// Pushes a `UndoCmd` to the top of the `UndoStack` and executes its `redo()` method.
+    /// This pops off all `UndoCmd`s that is above the active `UndoCmd` from the `UndoStack`.
     pub fn push(&mut self, mut cmd: T) {
         let is_dirty = self.is_dirty();
         self.stack.truncate(self.len);
         cmd.redo();
         self.stack.push(cmd);
-        self.len += 1;
+        self.len = self.stack.len();
         // Check if stack went from dirty to clean.
-        if is_dirty == self.is_clean() {
+        if is_dirty {
             if let Some(ref mut f) = self.on_clean {
                 f();
             }
         }
     }
 
+    /// Calls the `redo()` method for the active `UndoCmd` and sets the next `UndoCmd` as the new
+    /// active `UndoCmd`.
     pub fn redo(&mut self) {
-        let len = self.stack.len();
-        if len != 0 && self.len < len {
+        if self.len < self.stack.len() {
             let is_dirty = self.is_dirty();
+            self.len += 1;
             {
                 let ref mut cmd = self.stack[self.len - 1];
                 cmd.redo();
             }
-            self.len += 1;
             // Check if stack went from dirty to clean.
             if is_dirty == self.is_clean() {
                 if let Some(ref mut f) = self.on_clean {
@@ -135,6 +158,8 @@ impl<T: UndoCmd> UndoStack<T> {
         }
     }
 
+    /// Calls the `undo()` method for the active `UndoCmd` and sets the previous `UndoCmd` as the
+    /// new active `UndoCmd`.
     pub fn undo(&mut self) {
         if !self.stack.is_empty() {
             let is_clean = self.is_clean();
@@ -160,7 +185,7 @@ mod test {
     #[test]
     fn pop() {
         use std::rc::Rc;
-        use std::cell::RefCell;
+        use std::cell::{Cell, RefCell};
 
         /// Pops an element from a vector.
         #[derive(Clone)]
@@ -180,9 +205,13 @@ mod test {
             }
         }
 
+        let a = Cell::new(0);
+        let b = Cell::new(0);
         // We need to use Rc<RefCell> since all commands are going to mutate the vec.
         let vec = Rc::new(RefCell::new(vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
-        let mut undo_stack = UndoStack::with_capacity(3);
+        let mut undo_stack = UndoStack::with_capacity(3)
+            .on_clean(|| a.set(1))
+            .on_dirty(|| b.set(1));
 
         let cmd = PopCmd { vec: vec.clone(), e: None };
         undo_stack.push(cmd.clone());
@@ -198,7 +227,11 @@ mod test {
 
         assert!(vec.borrow().is_empty());
 
+        assert_eq!(b.get(), 0);
         undo_stack.undo();
+        assert_eq!(b.get(), 1);
+        b.set(0);
+
         undo_stack.undo();
         undo_stack.undo();
         undo_stack.undo();
@@ -210,5 +243,26 @@ mod test {
         undo_stack.undo();
 
         assert_eq!(vec, Rc::new(RefCell::new(vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9])));
+
+        assert_eq!(a.get(), 0);
+        undo_stack.push(cmd.clone());
+        assert_eq!(a.get(), 1);
+        a.set(0);
+
+        assert_eq!(vec, Rc::new(RefCell::new(vec![0, 1, 2, 3, 4, 5, 6, 7, 8])));
+
+        assert_eq!(b.get(), 0);
+        undo_stack.undo();
+        assert_eq!(b.get(), 1);
+        b.set(0);
+
+        assert_eq!(vec, Rc::new(RefCell::new(vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9])));
+
+        assert_eq!(a.get(), 0);
+        undo_stack.redo();
+        assert_eq!(a.get(), 1);
+        a.set(0);
+
+        assert_eq!(vec, Rc::new(RefCell::new(vec![0, 1, 2, 3, 4, 5, 6, 7, 8])));
     }
 }
