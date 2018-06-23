053c43b0de34d72ed4420cb7b7c7a6efe651211a
evenorog <evenorog@gmail.com>
2018-06-23 22:14:46 +0200

    Add path from dest to origin branch in jump_to

--- src/history.rs
+++ src/history.rs
@@ -1,4 +1,6 @@
+use std::collections::HashMap;
 use std::marker::PhantomData;
+use std::ops::RangeFrom;
 use {Command, Error, Record};
 
 #[derive(Debug)]
@@ -12,8 +14,9 @@ struct Branch<R> {
 #[derive(Debug)]
 pub struct History<R> {
     id: usize,
+    next: RangeFrom<usize>,
     record: Record<R>,
-    branches: Vec<Branch<R>>,
+    branches: HashMap<usize, Branch<R>>,
 }
 
 impl<R> History<R> {
@@ -22,8 +25,9 @@ impl<R> History<R> {
     pub fn new(receiver: impl Into<R>) -> History<R> {
         History {
             id: 0,
+            next: 1..,
             record: Record::new(receiver),
-            branches: Vec::new(),
+            branches: HashMap::new(),
         }
     }
 
@@ -36,20 +40,26 @@ impl<R> History<R> {
     /// [`apply`]: trait.Command.html#tymethod.apply
     /// [`id`]: trait.Command.html#method.id
     #[inline]
-    pub fn apply(&mut self, cmd: impl Command<R> + 'static) -> Result<(), Error<R>>
+    pub fn apply(&mut self, cmd: impl Command<R> + 'static) -> Result<Option<usize>, Error<R>>
     where
         R: 'static,
     {
         let commands = self.record.apply(cmd)?;
         let commands: Vec<_> = commands.collect();
         if !commands.is_empty() {
-            self.branches.push(Branch {
-                parent: self.id,
-                cursor: self.record.cursor(),
-                commands: commands.into_boxed_slice(),
-            });
+            let id = self.next.next().unwrap();
+            self.branches.insert(
+                id,
+                Branch {
+                    parent: self.id,
+                    cursor: self.record.cursor(),
+                    commands: commands.into_boxed_slice(),
+                },
+            );
+            Ok(Some(id))
+        } else {
+            Ok(None)
         }
-        Ok(())
     }
 
     /// Calls the [`undo`] method for the active command and sets the previous one as the new active one.
@@ -75,6 +85,29 @@ impl<R> History<R> {
     #[must_use]
     pub fn redo(&mut self) -> Option<Result<(), Error<R>>> {
         self.record.redo()
+    }
+
+    /// Jumps to the command in `branch` at `cursor`.
+    #[inline]
+    #[must_use]
+    pub fn jump_to(&mut self, branch: usize, cursor: usize) -> Option<Result<(), Error<R>>> {
+        if self.id == branch {
+            return self.record.set_cursor(cursor);
+        }
+
+        let mut dst = &self.branches[&branch];
+        let mut path = vec![dst];
+        while dst.parent != self.id {
+            dst = &self.branches[&dst.parent];
+            path.push(dst);
+        }
+
+        while let Some(last) = path.pop() {
+            let _ = last.parent;
+            let _ = last.cursor;
+        }
+
+        unimplemented!()
     }
 }
 
