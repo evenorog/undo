7192f42eeaf0d7c048b15c27dcd374a78f5830f8
evenorog <evenorog@gmail.com>
2018-12-15 15:38:37 +0100

    0.29.5 -> 0.29.6

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@
 //! [`merge!`]: macro.merge.html
 //! [`merge`]: trait.Command.html#method.merge
 
-#![doc(html_root_url = "https://docs.rs/undo/0.29.5")]
+#![doc(html_root_url = "https://docs.rs/undo/0.29.6")]
 #![deny(
     bad_style,
     bare_trait_objects,
