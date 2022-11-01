8c17ec335b4140e68146c7a2a1878fd1163ce93f
evenorog <evenorog@gmail.com>
2022-11-01 09:14:10 +0100

    Use Push actions instead of Add in examples

--- push.rs
+++ push.rs
@@ -1,16 +1,16 @@
-pub struct Add(char);
+pub struct Push(char);
 
-impl undo::Action for Add {
+impl undo::Action for Push {
     type Target = String;
     type Output = ();
     type Error = &'static str;
 
-    fn apply(&mut self, s: &mut String) -> undo::Result<Add> {
+    fn apply(&mut self, s: &mut String) -> undo::Result<Push> {
         s.push(self.0);
         Ok(())
     }
 
-    fn undo(&mut self, s: &mut String) -> undo::Result<Add> {
+    fn undo(&mut self, s: &mut String) -> undo::Result<Push> {
         self.0 = s.pop().ok_or("s is empty")?;
         Ok(())
     }
