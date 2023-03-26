8ef11882fb2051866b3d69188ebf2696a8566c6a
Even Olsson Rogstadkjærnet <evenorog@gmail.com>
2023-03-26 17:42:58 +0200

    Fix issues with history checkpoint causing panics

--- src/history/checkpoint.rs
+++ src/history/checkpoint.rs
@@ -2,7 +2,7 @@ use super::Queue;
 use crate::{Action, History, Slot};
 
 #[derive(Debug)]
-enum CheckpointAction {
+enum CheckpointEntry {
     Apply(usize),
     Undo,
     Redo,
@@ -12,7 +12,7 @@ enum CheckpointAction {
 #[derive(Debug)]
 pub struct Checkpoint<'a, A, S> {
     history: &'a mut History<A, S>,
-    actions: Vec<CheckpointAction>,
+    entries: Vec<CheckpointEntry>,
 }
 
 impl<A, S> Checkpoint<'_, A, S> {
@@ -31,19 +31,19 @@ impl<A: Action, S: Slot> Checkpoint<'_, A, S> {
     /// Calls the `apply` method.
     pub fn apply(&mut self, target: &mut A::Target, action: A) -> A::Output {
         let branch = self.history.branch();
-        self.actions.push(CheckpointAction::Apply(branch));
+        self.entries.push(CheckpointEntry::Apply(branch));
         self.history.apply(target, action)
     }
 
     /// Calls the `undo` method.
     pub fn undo(&mut self, target: &mut A::Target) -> Option<A::Output> {
-        self.actions.push(CheckpointAction::Undo);
+        self.entries.push(CheckpointEntry::Undo);
         self.history.undo(target)
     }
 
     /// Calls the `redo` method.
     pub fn redo(&mut self, target: &mut A::Target) -> Option<A::Output> {
-        self.actions.push(CheckpointAction::Redo);
+        self.entries.push(CheckpointEntry::Redo);
         self.history.redo(target)
     }
 
@@ -51,27 +51,26 @@ impl<A: Action, S: Slot> Checkpoint<'_, A, S> {
     pub fn commit(self) {}
 
     /// Cancels the changes and consumes the checkpoint.
-    pub fn cancel(self, target: &mut A::Target) -> Option<Vec<A::Output>> {
-        let mut outputs = Vec::new();
-        for action in self.actions.into_iter().rev() {
-            let output = match action {
-                CheckpointAction::Apply(branch) => {
+    pub fn cancel(self, target: &mut A::Target) -> Vec<A::Output> {
+        self.entries
+            .into_iter()
+            .rev()
+            .filter_map(|entry| match entry {
+                CheckpointEntry::Apply(branch) => {
                     let output = self.history.undo(target)?;
                     let root = self.history.branch();
-                    self.history.jump_to(branch);
                     if root == branch {
                         self.history.record.entries.pop_back();
                     } else {
+                        self.history.jump_to(branch);
                         self.history.branches.remove(&root).unwrap();
                     }
-                    output
+                    Some(output)
                 }
-                CheckpointAction::Undo => self.history.redo(target)?,
-                CheckpointAction::Redo => self.history.undo(target)?,
-            };
-            outputs.push(output);
-        }
-        Some(outputs)
+                CheckpointEntry::Undo => self.history.redo(target),
+                CheckpointEntry::Redo => self.history.undo(target),
+            })
+            .collect()
     }
 }
 
@@ -79,7 +78,41 @@ impl<'a, A, S> From<&'a mut History<A, S>> for Checkpoint<'a, A, S> {
     fn from(history: &'a mut History<A, S>) -> Self {
         Checkpoint {
             history,
-            actions: Vec::new(),
+            entries: Vec::new(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    const A: FromFn<fn(&mut String), String> = FromFn::new(|s| s.push('a'));
+    const B: FromFn<fn(&mut String), String> = FromFn::new(|s| s.push('b'));
+    const C: FromFn<fn(&mut String), String> = FromFn::new(|s| s.push('c'));
+    const D: FromFn<fn(&mut String), String> = FromFn::new(|s| s.push('d'));
+    const E: FromFn<fn(&mut String), String> = FromFn::new(|s| s.push('e'));
+
+    #[test]
+    fn checkpoint() {
+        let mut target = String::new();
+        let mut history = History::new();
+        let mut checkpoint = history.checkpoint();
+
+        checkpoint.apply(&mut target, A);
+        checkpoint.apply(&mut target, B);
+        checkpoint.apply(&mut target, C);
+        assert_eq!(target, "abc");
+
+        checkpoint.undo(&mut target);
+        checkpoint.undo(&mut target);
+        assert_eq!(target, "a");
+
+        checkpoint.apply(&mut target, D);
+        checkpoint.apply(&mut target, E);
+        assert_eq!(target, "ade");
+
+        checkpoint.cancel(&mut target);
+        assert_eq!(target, "");
     }
 }
