d01c3a5c9162b6d06a307f99aa44c04ba081ea20
Even Olsson Rogstadkjærnet <evenorog@gmail.com>
2023-09-21 21:20:48 +0200

    Make history example into an interactive client example

--- examples/history.rs
+++ examples/history.rs
@@ -1,38 +1,56 @@
 use chrono::{DateTime, Local};
+use std::io;
+use std::io::BufRead;
 use std::time::SystemTime;
-use undo::{Add, History};
+use undo::{Add, At, History};
 
 fn custom_st_fmt(_: SystemTime, at: SystemTime) -> String {
     let dt = DateTime::<Local>::from(at);
     dt.format("%H:%M:%S").to_string()
 }
 
-fn main() {
+fn main() -> io::Result<()> {
+    let stdin = io::stdin();
+    let mut stdin = stdin.lock();
+
     let mut target = String::new();
-    let mut history = History::new();
+    let mut record = History::new();
+    loop {
+        println!("Enter a string to edit. Use '-' to undo, '+' to redo, and '! i-j' for goto: ");
+        let mut string = String::new();
+        let n = stdin.read_line(&mut string)?;
+        if n == 0 {
+            return Ok(());
+        }
 
-    history.edit(&mut target, Add('a'));
-    history.edit(&mut target, Add('b'));
-    history.edit(&mut target, Add('c'));
-    assert_eq!(target, "abc");
+        // Clears the terminal.
+        print!("{}c", 27 as char);
 
-    let abc = history.head();
+        let mut chars = string.trim().chars();
+        while let Some(c) = chars.next() {
+            if c == '!' {
+                let rest = chars.collect::<String>();
+                let mut at = rest
+                    .trim()
+                    .split('-')
+                    .filter_map(|n| n.parse::<usize>().ok());
 
-    history.undo(&mut target);
-    assert_eq!(target, "ab");
+                if let (Some(root), Some(index)) = (at.next(), at.next()) {
+                    record.go_to(&mut target, At::new(root, index));
+                } else {
+                    println!("Expected input as '! i-j', e.g. '! 1-5'.\n");
+                }
+                break;
+            } else if c == '<' {
+                record.undo(&mut target);
+            } else if c == '>' {
+                record.redo(&mut target);
+            } else {
+                record.edit(&mut target, Add(c));
+            }
+        }
 
-    history.edit(&mut target, Add('d'));
-    history.edit(&mut target, Add('e'));
-    history.edit(&mut target, Add('f'));
-    assert_eq!(target, "abdef");
-
-    let abdef = history.head();
-
-    history.go_to(&mut target, abc);
-    assert_eq!(target, "abc");
-
-    history.go_to(&mut target, abdef);
-    assert_eq!(target, "abdef");
-
-    println!("{}", history.display().set_st_fmt(&custom_st_fmt));
+        println!("{}\n", record.display().set_st_fmt(&custom_st_fmt));
+        println!("Target: {target}");
+    }
 }
