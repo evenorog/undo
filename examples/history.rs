7749fa99cd833ab4b9993f75f057eba8ecd9235a
Even Olsson Rogstadkjærnet <evenorog@gmail.com>
2023-09-21 22:38:09 +0200

    Add save functionality to history example

--- examples/history.rs
+++ examples/history.rs
@@ -1,6 +1,5 @@
 use chrono::{DateTime, Local};
 use std::io;
-use std::io::BufRead;
 use std::time::SystemTime;
 use undo::{Add, At, History};
 
@@ -11,12 +10,13 @@ fn custom_st_fmt(_: SystemTime, at: SystemTime) -> String {
 
 fn main() -> io::Result<()> {
     let stdin = io::stdin();
-    let mut stdin = stdin.lock();
-
     let mut target = String::new();
-    let mut record = History::new();
+    let mut history = History::<_>::builder().limit(10).capacity(10).build();
+
     loop {
-        println!("Enter a string to edit. Use '-' to undo, '+' to redo, and '! i-j' for goto: ");
+        println!(
+            "Enter a string. Use '<' to undo, '>' to redo, '*' to save, and '! i-j' for goto: "
+        );
         let mut string = String::new();
         let n = stdin.read_line(&mut string)?;
         if n == 0 {
@@ -36,21 +36,23 @@ fn main() -> io::Result<()> {
                     .filter_map(|n| n.parse::<usize>().ok());
 
                 if let (Some(root), Some(index)) = (at.next(), at.next()) {
-                    record.go_to(&mut target, At::new(root, index));
+                    history.go_to(&mut target, At::new(root, index));
                 } else {
-                    println!("Expected input as '! i-j', e.g. '! 1-5'.\n");
+                    println!("Expected input as '! i-j', e.g. '! 1-5'.");
                 }
                 break;
             } else if c == '<' {
-                record.undo(&mut target);
+                history.undo(&mut target);
             } else if c == '>' {
-                record.redo(&mut target);
+                history.redo(&mut target);
+            } else if c == '*' {
+                history.set_saved(true);
             } else {
-                record.edit(&mut target, Add(c));
+                history.edit(&mut target, Add(c));
             }
         }
 
-        println!("{}\n", record.display().set_st_fmt(&custom_st_fmt));
+        println!("{}\n", history.display().set_st_fmt(&custom_st_fmt));
         println!("Target: {target}");
     }
 }
